b0f3d61324affad5e621fd9defdeda9693197653
oshan <oshxn998@gmail.com>
2023-11-08 09:15:40 +0530

    11/8 wednesday commit

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -4,6 +4,7 @@ import { Subject } from "rxjs";
 
 export class ShoppingListService{
     ingredientChanged=new Subject<Ingredient[]>();
+    startedEditing=new Subject<number>();
     private ingredients: Ingredient[]=[
         new Ingredient('apples',5),
         new Ingredient('tomato',10)
@@ -12,17 +13,30 @@ export class ShoppingListService{
       getIngredients(){
         return this.ingredients.slice();
       }
+      getIngredient(index: number){
+        return this.ingredients[index];
+      }
 
       addIngredients(ingredient: Ingredient){
         this.ingredients.push(ingredient);
         this.ingredientChanged.next(this.ingredients.slice());
       }
 
+
       addIngredientss(ingredients: Ingredient[]){
         // for(let ingredient of ingredients){
         //   this.addIngredients(ingredient);
         // }
         this.ingredients.push(...ingredients);
+        this.ingredientChanged.next(this.ingredients.slice());
+      }
+      updateIngredient(index: number, newIngredient:Ingredient){
+        this.ingredients[index]=newIngredient;
+        this.ingredientChanged.next(this.ingredients.slice());
+      }
+
+      deleteIngredient(index: number){
+        this.ingredients.splice(index,1);
         this.ingredientChanged.next(this.ingredients.slice());
       }
 }
